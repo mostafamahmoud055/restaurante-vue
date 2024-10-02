fabefb0295ad3fb68afb6e812207a11ab069eaad
mostafamahmoud055 <mostafamahmoud055@gmail.com>
2024-10-02 22:01:32 +0300

    edit v3

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,6 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
-  ? '/https://github.com/mostafamahmoud055/restaurante-vue/' // Replace with your repository name
-  : '/public'
+  ? '/https://github.com/mostafamahmoud055/restaurante-vue.git/' // Replace with your repository name
+  : '/'
 });
